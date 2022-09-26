b4c1dac29ccd8784e89ee7caaa030628bcf131fa
Lavender Glaab <piggehperson@gmail.com>
2022-09-26 18:27:45 -0400

    Use nicer color fallback macro

--- src/c/detail_page.h
+++ src/c/detail_page.h
@@ -1,5 +1,5 @@
 #include <pebble.h>
-#define DETAIL_BACKGROUND_COLOR PBL_IF_COLOR_ELSE(GColorGreen, GColorWhite)
+#define DETAIL_BACKGROUND_COLOR COLOR_FALLBACK(GColorGreen, GColorWhite)
 #define DETAIL_ON_BACKGROUND_COLOR GColorBlack
 
 #define LAYOUT_INSET_W PBL_IF_ROUND_ELSE(20, 6)
--- src/c/icon_test_page.h
+++ src/c/icon_test_page.h
@@ -1,5 +1,5 @@
 #include <pebble.h>
-#define ICON_TEST_BACKGROUND_COLOR PBL_IF_COLOR_ELSE(GColorYellow, GColorWhite)
+#define ICON_TEST_BACKGROUND_COLOR COLOR_FALLBACK(GColorYellow, GColorWhite)
 #define ICON_TEST_ON_BACKGROUND_COLOR GColorBlack
 
 static void icon_test_window_push();
